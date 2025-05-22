e500f5da3c9be4bee39b07a2ddb25b3786372669
sentinelle28 <augustesoulie@gmail.com>
2025-05-22 13:36:59 +0200

    Modified mouse and added basic starting game

--- instance/player_instance/mouse.gd
+++ instance/player_instance/mouse.gd
@@ -1,13 +1,13 @@
 extends Area2D
-
+class_name Player
 var last_selected_piece:Piece
+var is_black:bool = false
 @onready var anim:AnimatedSprite2D = $AnimatedSprite2D
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var mouse_pos:Vector2 = get_global_mouse_position()
-	global_position.x = lerpf(global_position.x,mouse_pos.x,delta)
-	global_position.y = lerpf(global_position.y,mouse_pos.y,delta)
-	
+	global_position.x = mouse_pos.x
+	global_position.y = mouse_pos.y
 	
 func can_click()->bool:
 	return is_instance_valid(last_selected_piece)
@@ -16,10 +16,16 @@ func can_click()->bool:
 
 func _on_area_entered(area: Area2D) -> void:
 	if area is Piece:
-		last_selected_piece = area
-		anim.play("can_click")
+		if area.is_black == is_black:
+			last_selected_piece = area
+			anim.play("can_click")
+			area.is_selected = true
+			area.queue_redraw()
 
 
 func _on_area_exited(area: Area2D) -> void:
-	last_selected_piece = null
-	anim.play("idle")
+	if area is Piece:
+		last_selected_piece = null
+		anim.play("idle")
+		area.is_selected = false
+		area.queue_redraw()
--- scene/gameplay_scene/gameplay.gd
+++ scene/gameplay_scene/gameplay.gd
@@ -6,17 +6,24 @@ var peer = ENetMultiplayerPeer.new()
 #@export var inputext:TextEdit
 const PORT:int = 135
 const MAX_PLAYER:int = 2
+var m_player:Player
+var is_two:bool = false
 
-
+@onready var action:VBoxContainer = $UI_related/UI/HBoxContainer/Action/InMatch
 
 func _on_host_pressed()->void:
 	peer.create_server(PORT,MAX_PLAYER)
 	multiplayer.multiplayer_peer = peer
+	multiplayer.connect("peer_connected",_update_peer)
 	_add_player(1)
 	_hide_UI()
 	
+func _update_peer(_value:int)->void:
+	is_two = true
+	
 func _hide_UI()->void:
 	internet_related.hide()
+	
 
 func _on_join_pressed()->void:
 	peer.create_client(IP.get_local_addresses()[1],PORT)
@@ -24,6 +31,8 @@ func _on_join_pressed()->void:
 	multiplayer.multiplayer_peer = peer
 	_add_player(2)
 	_hide_UI()
+	$UI_related/UI/HBoxContainer/Action/Start.hide()
+	
 
 func _add_player(id:int)->void:
 	var player = playerscene.instantiate()
@@ -31,3 +40,30 @@ func _add_player(id:int)->void:
 	print("created player with id:", id)
 	player.set_multiplayer_authority(id)
 	call_deferred("add_child",player)
+	m_player = player
+
+
+func _on_start_pressed() -> void:
+	if is_two:
+		$UI_related/UI/HBoxContainer/Action/Start.hide()
+		var is_black:bool = randi_range(0,1) == 1
+		var is_starting:bool = randi_range(0,1) == 1
+		action.visible = is_starting
+		m_player.is_black = is_black
+		
+		_change_color(is_black)
+		rpc("_start_game", not is_starting, not is_black)
+
+func is_peer_connected()->bool:
+	return get_multiplayer().peer.is_active()
+
+func _change_color(is_black:bool)->void:
+	if is_black:
+		$UI_related/UI/HBoxContainer/Play/ColorRect.color = Color.BLACK
+
+@rpc("call_remote","authority")
+func _start_game(is_starting:bool,is_black:bool)->void:
+	
+	action.visible = is_starting
+	m_player.is_black = is_black
+	_change_color(is_black)
