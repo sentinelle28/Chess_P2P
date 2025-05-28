85e39a4dbd45c3de2512ed3b9f53111473543d34
sentinelle28 <augustesoulie@gmail.com>
2025-05-28 10:15:04 +0200

    Rarity 2/2

--- autoload/ref/Card_lib/CardLib.gd
+++ autoload/ref/Card_lib/CardLib.gd
@@ -7,6 +7,28 @@ var array_of_card:Array[CardStrategyPattern] = [
 	AddKingCard.new()
 ]
 
+var array_of_rarity:Array[int] = [
+	6,
+	5,
+	5,
+	6
+]
+
+var total:int = 0
+
 func _ready() -> void:
 	for i:int in range(len(array_of_card)):
 		array_of_card[i].index = i
+		array_of_rarity[i] += total
+		total += array_of_rarity[i]
+		
+func get_random_card()->CardStrategyPattern:
+	var random_card:CardStrategyPattern
+	var num:int = randi_range(0,total)
+	var index:int = 0
+	for i:int in range(len(array_of_rarity)):
+		if num <= array_of_rarity[i]:
+			index = i
+			break
+	random_card = array_of_card[index]
+	return random_card
