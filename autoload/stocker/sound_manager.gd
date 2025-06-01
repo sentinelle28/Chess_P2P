e608a1a66ddaef159e05e44a7d06080d16b51baf
sentinelle28 <102462227+sentinelle28@users.noreply.github.com>
2025-06-01 11:52:34 +0200

    Added settings menu

--- autoload/stocker/sound_manager.gd
+++ autoload/stocker/sound_manager.gd
@@ -1,10 +1,13 @@
 extends AudioStreamPlayer
 
 func _ready() -> void:
-	_set_sound_level()
+	_set_main_sound_level()
 
-func _set_sound_level()->void:
+func _set_main_sound_level()->void:
 	volume_db = GameSettings.volume
+
+func _set_sfx_sound_level()->void:
+	pass
 
 
 func _on_finished() -> void:
--- scene/main/main_menu.gd
+++ scene/main/main_menu.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
 
+@onready var main_menu:CanvasLayer = $Main_menu
+@onready var setting_menu:CanvasLayer = $Settings
+
+@export var main_music_bar:HSlider
+@export var sfx_music_bar:HSlider
+
 func _on_button_pressed() -> void:
-	
 	_change_scene()
 
 
@@ -12,3 +17,25 @@ func _on_play_lan_pressed() -> void:
 
 func _change_scene()->void:
 	get_tree().change_scene_to_file("res://scene/gameplay_scene/gameplay.tscn")
+
+
+func _on_setting_button_pressed() -> void:
+	main_menu.hide()
+	setting_menu.show()
+
+func _on_quit_setting_pressed() -> void:
+	main_menu.show()
+	setting_menu.hide()
+
+
+func _on_main_music_slider_drag_ended(value_changed: bool) -> void:
+	if value_changed:
+		var new_value:float = main_music_bar.value
+		var rap:float = new_value/100.0
+		GameSettings.volume = (-80) * int(1.-rap)
+		SoundManager._set_main_sound_level()
+
+
+func _on_sfx_slider_drag_ended(value_changed: bool) -> void:
+	if value_changed:
+		pass
