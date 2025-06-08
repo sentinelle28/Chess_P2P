ab3ced40f448fbf465438a208cef2397b41e8aa4
sentinelle28 <augustesoulie@gmail.com>
2025-06-08 16:02:28 +0200

    Added new movement

---
+++ component/related_to_piece/piece_related/mouvement_component/RandomMoveOption.gd
@@ -0,0 +1,13 @@
+extends ComputeMouvOption
+class_name RandomMouvOption
+func get_compute_possible_mouv(tilemap:TileMapLayer,
+pos:Vector2i,
+array_of_piece:Array[Vector2i] )->Array[Vector2i]:
+	var array:Array[Vector2i] = []
+	var gameplay:GameplayScene = tilemap.get_tree().current_scene
+	var x:int = gameplay.get_randint(-3,3)
+	var y:int = gameplay.get_randint_seed(x,-3,3)
+	var posi:Vector2i = Vector2i(x,y)
+	if is_in_board(posi+pos,tilemap):
+		array.append(posi)
+	return array
