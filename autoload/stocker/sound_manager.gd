9e007a6a24cfe9a3bbe00db328f7159ef070f673
sentinelle28 <augustesoulie@gmail.com>
2025-06-13 10:00:56 +0200

    Change click sound

--- autoload/stocker/sound_manager.gd
+++ autoload/stocker/sound_manager.gd
@@ -9,10 +9,7 @@ func _set_main_sound_level()->void:
 
 func _set_sfx_sound_level()->void:
 	for child:AudioStreamPlayer in get_children():
-		if child.name == "Click":
-			child.volume_db = GameSettings.sfx_volume - 8.
-		else:
-			child.volume_db = GameSettings.sfx_volume - 2.
+		child.volume_db = GameSettings.sfx_volume - 2.
 
 
 func _on_finished() -> void:
